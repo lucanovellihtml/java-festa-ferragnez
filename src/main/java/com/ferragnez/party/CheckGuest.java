11dee38d036fbb51fb9cc6cd6fe0f743d67031e8
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-06-07 15:13:25 +0200

    add: added array and method to scanner a input

---
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,21 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+
+	public static void main(String[] args) {
+			
+		//INIZIALIZZAZIONE ARRAY
+		String []guestParty = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+	
+	
+		//METODO INPUT
+		Scanner input = new Scanner(System.in);
+		System.out.print(" - INSERISCI IL TUO NOME E COGNOME ---> ");
+		String userParty = input.nextLine();
+	
+	
+	}
+
+}
